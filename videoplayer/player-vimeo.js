56c42d4e436cdd0d9772f983ab271de88a6488ac
Aaron Castanos <AGCastanos@taylorscitechphil.com>
2017-11-17 17:53:09 +0800

    Add project files.

---
+++ videoplayer/player-vimeo.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    var myWindow = $("#window"),
+        undo = $("#undo");
+
+    undo.kendoButton();
+    undo.click(function () {
+        myWindow.data("kendoWindow").open();
+        undo.fadeOut();
+    });
+
+    function onClose() {
+        undo.fadeIn();
+    }
+
+    myWindow.kendoWindow({
+        width: "670px",
+        title: "Curtis1000 - NOP Commerce",
+        visible: false,
+        actions: [
+            "Pin",
+            "Minimize",
+            "Close"
+        ],
+        close: onClose
+    }).data("kendoWindow").center().open();    
+
+    var player = new Vimeo.Player('made-in-ny');
+
+    player.setVolume(0.2);
+
+    player.on('play', function () {
+        console.log('played the video!');
+    });
+});
